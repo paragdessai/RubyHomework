e021f44e4cb499a91248cf7f27f3e5ba86cd521f
Parag Dessai <paragdesai123@gmail.com>
2017-07-17 21:38:10 -0400

    v1

---
+++ Homework1/Homework1.rb
@@ -0,0 +1,14 @@
+# Create a method to add two parameters, adds them together, and returns the result.
+
+def add(a,b)
+	a+b
+	end
+puts add(1,2)
+
+#method called join_strings that takes two string values, joins them together with a space in between, and returns the result. For example, join_strings('Hello', 'Dolly!') should return "Hello Dolly!".
+
+def join(a,b)
+	a + ' ' + b
+	end
+
+	puts join('Hello','dolly')
